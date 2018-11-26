fa261ca63c267d9b6c24991d16f2a244f862e28e
RGaius <15528330581@163.com>
2018-11-26 21:56:31 +0800

    feat:add loaclStorage

---
+++ src/renderer/common/YuQueStorage.js
@@ -0,0 +1,38 @@
+const storage = require('electron-json-storage');
+import Constant from '../common/constant'
+
+export default class YuQueStorage{
+    /**
+     * 构造函数
+     */
+    constructor() {
+
+    }
+    /**
+     * 缓存语雀的登录信息到本地
+     * @param {string} param
+     * @param {function} callback
+     */
+    saveYuqueInfo(param, callback){
+        let params = {
+            token : param.token,
+            name : param.authName
+        }
+        await this.setYuqueStorage(Constant.yuque.infoKey, sparams)
+        callback && callback()
+    }
+
+    /**
+     * 缓存信息到本地
+     * @param {string} key 本地缓存key
+     * @param {object} value 本地缓存value
+     */
+    setYuqueStorage(key, value) {
+        return new Promise(function (resolve, reject) {
+            storage.set(key, value, function(error){
+                if (error) return reject(error);
+                resolve();
+            })
+        })
+    }   
+}
---
+++ src/renderer/common/constant.js
@@ -0,0 +1,7 @@
+export default {
+    yuque: {
+        // 语雀登录信息缓存本地的key
+        infoKey : 'yuque_key'
+    }
+    
+}
